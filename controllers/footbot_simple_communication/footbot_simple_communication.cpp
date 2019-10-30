9edbaca946f10c662efdd2e162a29f57943f128f
Yihan Zhang <shzhangyihan@gmail.com>
2019-10-30 00:48:25 -0500

    simple comm working

---
+++ controllers/footbot_simple_communication/footbot_simple_communication.cpp
@@ -0,0 +1,62 @@
+/* Include the controller definition */
+#include "footbot_simple_communication.h"
+/* Function definitions for XML parsing */
+#include <argos3/core/utility/configuration/argos_configuration.h>
+
+/****************************************/
+/****************************************/
+
+CFootBotSimpleCommunication::CFootBotSimpleCommunication() :
+   m_pcRABA(NULL),
+   m_pcRABS(NULL),
+   ProbRange(0.0f, 1.0f),
+   m_pcRNG(NULL) {}
+
+/****************************************/
+/****************************************/
+
+void CFootBotSimpleCommunication::Init(TConfigurationNode& t_node) {
+   try {
+      /*
+       * Initialize sensors/actuators
+       */
+      m_pcRABA      = GetActuator<CCI_RangeAndBearingActuator     >("range_and_bearing"    );
+      m_pcRABS      = GetSensor  <CCI_RangeAndBearingSensor       >("range_and_bearing"    );
+      m_pcRNG = CRandom::CreateRNG("argos");
+
+      my_id = int(255 * m_pcRNG->Uniform(ProbRange));
+      printf("buf size = %d\n", m_pcRABA->GetSize());
+      printf("id = %d\n", my_id);
+      m_pcRABA->SetData(0, my_id);
+   }
+   catch(CARGoSException& ex) {
+      THROW_ARGOSEXCEPTION_NESTED("Error initializing the foot-bot foraging controller for robot \"" << GetId() << "\"", ex);
+   }
+}
+
+/****************************************/
+/****************************************/
+
+void CFootBotSimpleCommunication::ControlStep() {
+    const CCI_RangeAndBearingSensor::TReadings& tPackets = m_pcRABS->GetReadings();
+    printf("%d recved: [", my_id);
+    for(size_t i = 0; i < tPackets.size(); ++i) {
+        printf("%d ", tPackets[i].Data[0]);
+    }
+    printf("]\n");
+}
+
+/****************************************/
+/****************************************/
+
+/*
+ * This statement notifies ARGoS of the existence of the controller.
+ * It binds the class passed as first argument to the string passed as
+ * second argument.
+ * The string is then usable in the configuration file to refer to this
+ * controller.
+ * When ARGoS reads that string in the configuration file, it knows which
+ * controller class to instantiate.
+ * See also the configuration files for an example of how this is used.
+ */
+REGISTER_CONTROLLER(CFootBotSimpleCommunication, "footbot_simple_communication_controller")
