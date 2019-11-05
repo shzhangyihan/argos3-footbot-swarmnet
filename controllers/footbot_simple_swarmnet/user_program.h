6d9edaad7281b14f7aa381c974dec7a1f74563a5
shzhangyihan <shzhangyihan@gmail.com>
2019-11-05 17:02:54 -0600

    simple sn working, time chan bug

--- controllers/footbot_simple_swarmnet/user_program.h
+++ controllers/footbot_simple_swarmnet/user_program.h
@@ -1,39 +1,32 @@
 START_USER_PROGRAM
 
-int motor_state;
+Channel *channel_1;
+Publisher *publisher_1;
+Subscriber *subscriber_1; 
+unsigned char token[5];
+int rand_id;
 
+void sent() {
+    token[1] = token[1] + 1;
+    publisher_1->send(token, strlen((char*)token));
+}
+
+void call_back(unsigned char * msg, int size, int hop, Meta_t * meta) {
+    printf("%d: Recv dist = %d, size = %d, counter = %d", rand_id, meta->dist, size, (int)msg[1]);
+    printf(", from = %d, delay = %d\r\n", (int)msg[0], meta->msg_delay);
+} 
 void loop() {
-    printf("clock: %d motion: %d\n", swarmos.get_clock(), motor_control->current_status());
-    if(motor_control->current_status() == Stop) {
-        if(motor_state == 0) {
-            motor_control->turn_right(50);
-            LED_control->turn_on(0, 255, 0, 50);
-            motor_state = 1;
-            printf("left\n");
-        }
-        else if(motor_state == 1) {
-            motor_control->move_forward(50);
-            LED_control->turn_on(255, 255, 255, 50);
-            motor_state = 2;
-            printf("forward\n");
-        }
-        else if(motor_state == 2) {
-            motor_control->turn_left(50);
-            LED_control->turn_on(255, 0, 0, 50);
-            motor_state = 3;
-            printf("right\n");
-        }
-        else if(motor_state == 3) {
-            motor_control->move_forward(50);
-            LED_control->turn_on(255, 255, 255, 50);
-            motor_state = 0;
-            printf("forward\n");
-        }
-    }
 }
 
 void setup() {
-    motor_state = 0;
+    rand_id = rand() % 255;
+    strncpy((char*)token, " Hello world!", 5);
+    token[0] = rand_id;
+    token[1] = 0;
+    channel_1 = swarmnet->new_channel(1, 0, true);
+    publisher_1 = channel_1->new_publisher(sent);
+    subscriber_1 = channel_1->new_subscriber(100, call_back);
+    publisher_1->send(token, strlen((char*)token));
 }
 
 END_USER_PROGRAM
---
+++ controllers/footbot_sn_firefly/user_program.h
@@ -0,0 +1,68 @@
+#pragma once
+#include <math.h>
+
+START_USER_PROGRAM
+
+#define DEFAULT_PERIOD 150
+#define LED_DURATION 2
+#define TIMER_CHANGE_BOUND 10
+#define NORMALIZE_FACTOR 20
+
+typedef struct state {
+    int period_length;
+} state_t;
+
+Channel * channel;
+Publisher * publisher;
+Subscriber * subscriber;
+state_t my_state;
+int last_rest;
+
+void sent_callback() {
+    printf("sent\n");
+}
+
+void recv_callback(unsigned char * msg, int size, int ttl, Meta_t * meta) {
+    // subtracting the queueing delay
+    // here my_time is the time when the sender called send function
+    int my_time = swarmos.get_clock() - last_rest - meta->msg_delay;
+    if(my_time < 0) {
+        my_time = DEFAULT_PERIOD + my_time;
+    }
+    printf("my_time = %d last_rest = %d delay = %d\n", my_time, last_rest, meta->msg_delay);
+    if(my_time < (my_state.period_length / 2))
+        my_state.period_length = my_state.period_length + my_time / NORMALIZE_FACTOR;
+    else
+        my_state.period_length = my_state.period_length - (my_state.period_length - my_time) / NORMALIZE_FACTOR;
+    
+    if(my_state.period_length < DEFAULT_PERIOD - TIMER_CHANGE_BOUND)
+        my_state.period_length = DEFAULT_PERIOD - TIMER_CHANGE_BOUND;
+    if(my_state.period_length > DEFAULT_PERIOD + TIMER_CHANGE_BOUND)
+        my_state.period_length = DEFAULT_PERIOD + TIMER_CHANGE_BOUND;
+}
+
+void loop() {
+    if(swarmos.get_clock() - last_rest > my_state.period_length) {
+        LED_control->turn_on(255, 255, 255, LED_DURATION);
+        publisher->send((unsigned char *) &my_state, sizeof(my_state));
+        last_rest = swarmos.get_clock();
+        printf("%d\n", my_state.period_length);
+        my_state.period_length = DEFAULT_PERIOD;
+    }
+    if(LED_control->current_status() == Off) {
+        LED_control->turn_on(0, 0, 255, 10000);
+    }
+}
+
+void setup() {
+    channel = swarmnet->new_channel(0, 0, true);
+    publisher = channel->new_publisher(sent_callback);
+    subscriber = channel->new_subscriber(200, recv_callback);
+
+    // my_state.period_length = DEFAULT_PERIOD;
+    my_state.period_length = swarmos.random_func() % DEFAULT_PERIOD;
+    last_rest = swarmos.get_clock();
+    LED_control->turn_on(0, 0, 255, 10000);
+}
+
+END_USER_PROGRAM
